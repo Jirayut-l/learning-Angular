5c56265f24430ff0ee145553f4798642f526a440
jirayut.l <jirayut.l@outlook.co.th>
2022-05-17 15:16:20 +0700

    add component to home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,13 +6,22 @@ import {RouterModule} from "@angular/router";
 import {ComponentsModule} from "../../shared/components/components.module";
 import  {InputOutputComponent} from "./components/input-output";
 import {AliasingComponent} from "./pages/input-output/aliasing";
-
+import  {SizerComponent} from "./pages/input-output/sizer";
+import {ItemOutputComponent} from "./pages/input-output/item-output";
+import {ItemDetailComponent} from "./pages/input-output/item-detail";
+import  {InTheMetadataComponent} from "./pages/input-output/in-the-metadata";
+import  {ItemInputOutputComponent} from "./pages/input-output/item-input-output";
 
 @NgModule({
   declarations: [
     HomeLayoutComponent,
     InputOutputComponent,
-    AliasingComponent
+    AliasingComponent,
+    SizerComponent,
+    ItemOutputComponent,
+    ItemDetailComponent,
+    InTheMetadataComponent,
+    ItemInputOutputComponent
   ],
   imports: [
     CommonModule,
