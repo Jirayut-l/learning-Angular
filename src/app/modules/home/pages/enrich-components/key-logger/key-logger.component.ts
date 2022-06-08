8c2b1bc536b811d96360c4e8c22c813092f51f1c
jirayut.l <jirayut.l@outlook.co.th>
2022-06-08 13:33:53 +0700

    add key logger component

---
+++ src/app/modules/home/pages/enrich-components/key-logger/key-logger.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { filter, fromEvent, map, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-key-logger',
+  templateUrl: './key-logger.component.html',
+  styleUrls: ['./key-logger.component.scss']
+})
+export class KeyLoggerComponent implements OnInit {
+  keys: string = '';
+  @Input() public numeric: boolean = false;
+  @ViewChild('keyContainer', { static: true }) public input: ElementRef<HTMLInputElement> = {} as ElementRef;
+
+  ngOnInit(): void {
+    const logger = fromEvent<KeyboardEvent>(this.input.nativeElement, 'keyup');
+    logger.pipe(
+      map((evt: KeyboardEvent) => evt.key.charCodeAt(0)),
+      filter(code => {
+        if (this.numeric) {
+          return !(code > 31 && (code < 48 || code > 57));
+        }
+        return true;
+      }),
+      tap(digit => this.keys += String.fromCharCode(digit))
+    ).subscribe();
+  }
+}
